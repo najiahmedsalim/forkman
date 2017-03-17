e12ecc66eddc936eaad3c7b93129e3d34ee9441e
anaji <anaji@gmlin153>
2017-03-17 10:27:52 +0100

    fgvn

---
+++ relou/c.cpp
@@ -0,0 +1,18 @@
+/*
+ * c.cpp
+ *
+ *  Created on: 17 mars 2017
+ *      Author: anaji
+ */
+
+#include "c.h"
+
+c::c() {
+	// TODO Auto-generated constructor stub
+
+}
+
+c::~c() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ relou/c.h
@@ -0,0 +1,17 @@
+/*
+ * c.h
+ *
+ *  Created on: 17 mars 2017
+ *      Author: anaji
+ */
+
+#ifndef C_H_
+#define C_H_
+
+class c {
+public:
+	c();
+	virtual ~c();
+};
+
+#endif /* C_H_ */
